92795d37e6c01e0a9ef90b3d85e39a0d8647ea07
mgregory-carecloud <mgregory@carecloud.com>
2017-10-11 14:50:04 -0400

    Better error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,9 @@ class ApplicationController < ActionController::Base
     flash[:alert] = "Page not found!"
     redirect_to new_excel_path
   end
+  
+  def valid_id?(id)
+    id.to_i.to_s == id.to_s
+  end
 
 end
--- app/controllers/excel_controller.rb
+++ app/controllers/excel_controller.rb
@@ -6,7 +6,7 @@ class ExcelController < ApplicationController
 
   def show
     @excel = Excel.find_by(id: params[:id])
-    unless @excel
+    unless valid_id?(params[:id]) && @excel
       flash[:alert] = "There's no excel file with that id!"
       redirect_to new_excel_path
     end
@@ -27,5 +27,5 @@ class ExcelController < ApplicationController
   def excel_params
     {file: params.dig(:excel, :excel_file).try(:tempfile)}
   end
-  
+
 end
